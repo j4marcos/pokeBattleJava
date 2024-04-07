abc3518bf61a31ec82262e4f9c7690ac83faa8d5
Puca Vaz <pucantc@gmail.com>
2024-04-07 15:25:28 -0300

    feature: add font do jogo para os botões de ação

--- interfaceCaixas/Options.java
+++ interfaceCaixas/Options.java
@@ -1,6 +1,12 @@
 package interfaceCaixas; 
 
 import java.awt.GridLayout;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Font;
+import java.awt.FontFormatException;
+
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -20,6 +26,13 @@ public class Options extends JPanel {
 
     public void botao(String nome) {
         JButton botao = new JButton(nome);
+
+        try {
+            Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("assents/font/pokemon_fire_red.ttf"));
+            botao.setFont(customFont.deriveFont(Font.PLAIN, 22)); 
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
         botao.addActionListener(e -> {
             if (nome.equals("FIGHT")) {
                 System.out.println(nome + " apertado!");
