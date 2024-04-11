7cd4d5a6130d5977cd36076f3a3ce684eb95ddff
rafaelfranca1 <rafaeldefranca750@gmail.com>
2024-04-10 22:11:13 -0300

    bag class

--- Bag.java
+++ Bag.java
@@ -9,28 +9,29 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class Bag extends JPanel {
+    JLabel background = new JLabel();
 
     public Bag() {
         editar();
     }
 
     public void editar() {
-        JLabel background = new JLabel();
         background.setFocusable(true);
-        background.setIcon(new ImageIcon("assents/backgroundsImages/base-itens.png"));
+        background.setIcon(new ImageIcon("assents/backgroundImages/cabase-itens.png"));
         JScrollPane scrollPane = listaDeItens();
         background.add(scrollPane);
-        background.add(itemAtual());
 
         add(background);
     }
 
     public JLabel itemAtual() {
         JLabel itemAtual = new JLabel();
-        itemAtual.setIcon(new ImageIcon("assents/backgroundsImages/super-potion.png")); // link do sprite: https://github.com/msikma/pokesprite/blob/master/items-outline/medicine/super-potion.png
-        itemAtual.setBounds(25, 460, 98, 98);
+        itemAtual.setIcon(new ImageIcon("assents/backgroundImages/super-potion.png")); // link do sprite: https://github.com/msikma/pokesprite/blob/master/items-outline/medicine/super-potion.png
+        itemAtual.setBounds(15, 460, 98, 98);
 
         return itemAtual;
     }
@@ -38,7 +39,7 @@ public class Bag extends JPanel {
     public JScrollPane listaDeItens() {
         DefaultListModel<String> modeloLista = new DefaultListModel<>();
         for (int i = 0; i < 20; i++) {
-            modeloLista.addElement("Item " + (i + 1));
+            modeloLista.addElement(String.format("ITEM %02d", (i + 1)));
         }
 
         JList<String> listaDeItens = new JList<>(modeloLista);
@@ -48,12 +49,26 @@ public class Bag extends JPanel {
         JScrollPane scrollPane = new JScrollPane(listaDeItens);
         scrollPane.setBounds(340, 30, 520, 350);
 
-        scrollPane.addMouseWheelListener(new MouseWheelListener() { // bug corrigido
+        scrollPane.addMouseWheelListener(new MouseWheelListener() {
             public void mouseWheelMoved(MouseWheelEvent e) {
                 repaint(); 
             }
         });
 
+        listaDeItens.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()) {
+                    if (listaDeItens.getSelectedIndex() != -1) {
+                        JLabel item = new JLabel(String.format("%s is selected.", listaDeItens.getSelectedValue()));
+                        item.setBounds(180, 460, 400, 100);
+                        item.setFont(new Font("Arial", Font.BOLD, 40));
+                        background.setIcon(new ImageIcon("assents/backgroundImages/nova-base.png"));
+                        background.add(item);
+                        background.add(itemAtual());
+                    }
+                }
+            }
+        });
         
 
         scrollPane.setBackground(new Color(0, 0, 0, 0)); // Tornando o JScrollPane transparente
