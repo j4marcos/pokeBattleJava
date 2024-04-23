85001702f684292bffc81bcc2bc4f1b1028e674f
Rafael França <rafaeldefranca750@gmail.com>
2024-04-23 17:01:38 -0300

    Update Bag.java

--- Bag.java
+++ Bag.java
@@ -28,33 +28,6 @@ public class Bag extends JPanel {
         editar(frame);
     }
 
-    public Bag() {
-        setLayout(new BorderLayout());
-        background.setFocusable(true);
-        background.setIcon(new ImageIcon("assets/backgroundImages/base-itens.png"));
-        JScrollPane scrollPane = listaDeItens();
-        background.add(scrollPane);
-        background.revalidate();
-
-        // JLabel backButtonBackground = new JLabel();
-        // backButtonBackground.setIcon(new ImageIcon("assets/backgroundImages/botao voltar.png"));
-        // backButtonBackground.setBounds(10, 300, 308, 116);
-
-        JButton backButton = new JButton("Voltar");
-        backButton.addActionListener(e -> {
-            // PokemonsBatle pokemonsBatle = new PokemonsBatle(frame);
-            // frame.mudarTela(pokemonsBatle);
-            System.out.println("Voltar apertado!");
-        });
-        // backButton.setContentAreaFilled(false);
-        // backButton.setBorderPainted(false);
-
-        backButton.setBounds(13, 117, 308, 116);
-        background.add(backButton);
-
-        add(background, BorderLayout.NORTH);
-    }
-
     private void editar(Game frame) {
         setLayout(new BorderLayout());
         background.setFocusable(true);
@@ -63,21 +36,16 @@ public class Bag extends JPanel {
         background.add(scrollPane);
         background.revalidate();
 
-        JLabel backButtonBackground = new JLabel();
-        backButtonBackground.setIcon(new ImageIcon("assets/backgroundImages/botao voltar.png"));
-        backButtonBackground.setBounds(10, 10, 100, 40);
-
-        JButton backButton = new JButton("Voltar");
+        JButton backButton = new JButton(new ImageIcon("assets/backgroundImages/botao voltar.png"));
         backButton.addActionListener(e -> {
             PokemonsBatle pokemonsBatle = new PokemonsBatle(frame);
             frame.mudarTela(pokemonsBatle);
             System.out.println("Voltar apertado!");
         });
-        backButton.setContentAreaFilled(false);
         backButton.setBorderPainted(false);
 
-        backButtonBackground.add(backButton);
-        background.add(backButtonBackground);
+        backButton.setBounds(244, 120, 76, 76);
+        background.add(backButton);
 
         add(background, BorderLayout.NORTH);
     }
